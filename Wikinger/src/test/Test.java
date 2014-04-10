b2d00118d90df0a90ff60d26cb7699ec635075fe
frudisch <FRudisch@gmx.de>
2014-04-10 13:58:35 +0200

    testCommit

--- Wikinger/src/test/Test.java
+++ Wikinger/src/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
 	
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt es geht wieder!");
+		System.out.println("Hallo Welt es geht wieder!!?!!!");
 	}
 
 }
