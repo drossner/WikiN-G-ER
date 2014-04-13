e08b717175efbc0e465c157ed53429afe0d14e92
frudisch <FRudisch@gmx.de>
2014-04-13 20:30:28 +0200

    main-Programm

--- Wikinger/src/part/offline/test/GazetterLoadTest.java
+++ Wikinger/src/part/offline/test/GazetterLoadTest.java
@@ -4,9 +4,13 @@ import part.offline.data.Gazetter;
 
 public class GazetterLoadTest {
 	
+	public static void main(String []Args){
+		new GazetterLoadTest();
+	}
+	
 	public GazetterLoadTest() {
 		String arr[];
-		Gazetter gaz = new Gazetter("");
+		Gazetter gaz = new Gazetter("./Gazetter.csv");
 		arr = gaz.loadGaz();
 		for (int i = 0; i<arr.length; i++){
 			System.out.println(i + " Stelle: " + arr[i]);
