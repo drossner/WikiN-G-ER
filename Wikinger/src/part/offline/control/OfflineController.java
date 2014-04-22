da9e3d8c1a3b80e83a8ac62c289d529e7c7e2577
frudisch <FRudisch@gmx.de>
2014-04-22 22:36:23 +0200

    loadOldGaz

--- Wikinger/src/part/offline/control/OfflineController.java
+++ Wikinger/src/part/offline/control/OfflineController.java
@@ -1,8 +1,10 @@
 package part.offline.control;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 
+import data.control.FileInput;
 import data.control.StanfordNER;
 
 public class OfflineController {
@@ -11,14 +13,12 @@ public class OfflineController {
 	int cities;
 	StanfordNER ner;
 	
-	public OfflineController(StanfordNER ner){
-		ArrayList<String> temp = new ArrayList<String>(gaz.loadGazetteerforCrawler());
-		uniqueCityNames = new String[temp.size()];
-		temp.toArray(uniqueCityNames);
+	public OfflineController(StanfordNER ner, String fileNameOldGazetteer){
+		String [] uniqueCityNames = loadGazetter(fileNameOldGazetteer);
 		cities = uniqueCityNames.length;
 		this.ner = ner;
 	}
-	
+
 	/**
 	 * Method starts crawling with given parameters on a wiki MySQL-database
 	 * @param threads Threads which are crawling
@@ -46,13 +46,13 @@ public class OfflineController {
 		int rest = cities%threads;
 		
 			for (int i = 0; i < threads-1; i++) {
-				CrawlerUnit temp = new CrawlerUnit(uniqueCityNames, counter, counter+step-1, gaz, connectors[i], ner, i);
+				CrawlerUnit temp = new CrawlerUnit(uniqueCityNames, counter, counter+step-1, connectors[i], ner, i);
 				threadList[i] = new Thread(temp);
 				threadList[i].start();
 				counter += step;	
 			}
 			
-			CrawlerUnit temp = new CrawlerUnit(uniqueCityNames, counter, counter+step-1+rest, gaz, connectors[threads-1], ner, threads-1);
+			CrawlerUnit temp = new CrawlerUnit(uniqueCityNames, counter, counter+step-1+rest, connectors[threads-1], ner, threads-1);
 			threadList[threads-1] = new Thread(temp);
 			threadList[threads-1].start();
 			
@@ -67,6 +67,26 @@ public class OfflineController {
 		}
 		
 		
+	}
+	
+
+	private String[] loadGazetter(String fileName) {
+		HashMap<String, String> rc = new HashMap<String, String>();
+		FileInput in = new FileInput(fileName);
+		String[] temp = in.loadCompleteFile();
+		String[] line = null;
+		String value = new String();
+		
+		for(int i = 0; i<temp.length; i++){
+			line = temp[i].split(",");	//Gazetteer speziell
+			value = line[1];			//Gazetteer speziell
+			
+			if(rc.containsKey(value)){
+				rc.put(value, value);
+			}
+		}
+		
+		return rc.keySet().toArray(new String[rc.size()]);
 	}
 
 }
