c0209c67816bc57e7b27dbc2c06d507e8abfe6fa
Mario <mario.kuepferling@hof-university.de>
2014-04-30 10:43:09 +0200

    Offline Gui

--- Wikinger/src/part/offline/view/OfflineGui.java
+++ Wikinger/src/part/offline/view/OfflineGui.java
@@ -1,7 +1,10 @@
 package part.offline.view;
 
 import java.awt.Dimension;
+import java.awt.LayoutManager;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -9,13 +12,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
+import edu.stanford.nlp.ling.CoreAnnotations.YearAnnotation;
+
 public class OfflineGui extends JFrame{
 
 	/**
 	 * Computed by JVM
 	 */
 	private static final long serialVersionUID = -1729904057278086015L;
-	private JPanel mainPanel;
+	private JPanel mainPanel, northpanel, southpanel;
 	private JButton startButton;
 	private JComboBox<String> dbselector;
 	private String[] suitableDB = {"mySql-WikiNER", "Oracle 11g Release"};
@@ -31,13 +36,15 @@ public class OfflineGui extends JFrame{
 		super("Offline Part - WikiNER");
 		this.createOfflineFrame();
 		this.setVisible(true);
-		this.setLocation(300, 200);
+		this.setLocation(350, 180);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	private void createOfflineFrame(){
-		this.setSize(new Dimension(400, 300));
+		this.setSize(new Dimension(550, 400));
 		this.mainPanel = new JPanel();
+		this.northpanel = new JPanel();
+		this.southpanel = new JPanel();
 		this.startButton = new JButton("Start Process");
 		//JUST FOR TESTING!
 		this.dbselector = new JComboBox<>(suitableDB);
@@ -49,7 +56,19 @@ public class OfflineGui extends JFrame{
 		this.addingProgress = new JProgressBar(0, 100);
 		this.successfulOffline = new JLabel("Offline Part successfull");
 		
-		//TODO: set Layout for MainPanel and configure Actions!
+		//TODO:add Components, set Layout for MainPanel and configure Actions!
+		this.add(mainPanel);
+		mainPanel.add(northpanel);
+		mainPanel.add(southpanel);
+		northpanel.add(startButton);
+		northpanel.add(dbselector);
+		northpanel.add(crawlDB);
+		northpanel.add(crawlProgress);
+		southpanel.add(successfulCrawl);
+		southpanel.add(successfulCreateFile);
+		southpanel.add(readAndAddFiletoDB);
+		southpanel.add(addingProgress);
+		southpanel.add(successfulOffline);
 	}
 	
 }
