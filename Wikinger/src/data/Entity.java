0d4aaf9459f31b22a8387fe7a10dba35eabe32c1
Mario <mario.kuepferling@hof-university.de>
2014-05-02 12:16:58 +0200

    Entity @return

--- Wikinger/src/data/Entity.java
+++ Wikinger/src/data/Entity.java
@@ -4,7 +4,21 @@ public class Entity {
 	
 	private String name;
 	private int count;
+	private String type;
 	
+	public Entity(String name, String type){
+		this.name = name;
+		this.type = type;
+	}
+	
+	public String getType(){
+		return type;
+	}
+
+	public void setType(String type){
+		this.type = type;
+	}
+
 	public String getName() {
 		return name;
 	}
