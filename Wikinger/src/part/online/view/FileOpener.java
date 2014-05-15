93ccd1df46e9ae2fe346e60117e835c176149556
Mario <mario.kuepferling@hof-university.de>
2014-05-15 11:46:56 +0200

    Fileopener commited

---
+++ Wikinger/src/part/online/view/FileOpener.java
@@ -0,0 +1,50 @@
+package part.online.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileOpener implements ActionListener{
+
+	private JTextField textField;
+	
+	public FileOpener(JTextField textFeld){
+		this.textField = textFeld;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event){
+		String actionTodo = event.getActionCommand();
+		if(actionTodo.equals("browse")){
+			showFileChooser(new FileNameExtensionFilter("Textfiles (*.txt)", "txt"));
+		}
+		
+		else if(actionTodo.equals("readclassifier")){
+			showFileChooser(new FileNameExtensionFilter("Classifiers Files (*.tar)", "gz"));
+		}
+		
+	}
+
+	private void showFileChooser(FileNameExtensionFilter filter){
+		JFileChooser chooser = new JFileChooser("./");
+		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		chooser.setFileFilter(filter);
+		
+		int resultOpen = chooser.showOpenDialog(null);
+		
+		if(resultOpen == JFileChooser.APPROVE_OPTION){
+			String absPath = chooser.getSelectedFile().getAbsolutePath();
+			textField.setText(absPath);
+		}
+		else if(resultOpen == JFileChooser.CANCEL_OPTION){
+			chooser.cancelSelection();
+		}
+		
+	}
+	
+
+}
+
