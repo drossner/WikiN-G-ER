35bb95f6b8c9531c8aa8cf5bc8e700377010831d
frudisch <FRudisch@gmx.de>
2014-06-04 22:08:29 +0200

    test noch nicht mitcommited

---
+++ Wikinger/src/test/CrawlingTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.sql.SQLException;
+
+import data.control.StanfordNER;
+import part.offline.control.OfflineController;
+import part.offline.control.Status;
+
+public class CrawlingTest {
+
+	public static void main(String[] args) {
+		Status s;
+		OfflineController c = new OfflineController(new StanfordNER("./classifiers/english.muc.7class.distsim.crf.ser.gz"), "./gazetteer.csv");
+		try {
+			s = c.init(8);
+			OfflineControllerTestGui gui = new OfflineControllerTestGui(s);
+			gui.init();
+			c.startCrawling("localhost", 3306, "wikiDump", "root", "sner");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
