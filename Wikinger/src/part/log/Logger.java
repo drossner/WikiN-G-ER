83054059c9ee17bf453ef7f47fda985271cd4170
frudisch <FRudisch@gmx.de>
2014-06-05 11:12:00 +0200

    calculate hinzugefügt

--- Wikinger/src/part/log/Logger.java
+++ Wikinger/src/part/log/Logger.java
@@ -16,26 +16,30 @@ public class Logger implements Runnable{
 	public Logger(String pathName, Status status) {
 		this.status = status;
 		this.out = new FileOutput(true, pathName);
-		format = new SimpleDateFormat("dd.mm.yyyy HH.mm.ss");
+		format = new SimpleDateFormat("dd.mm.yyyy HH:mm:ss");
 	}
 
 	public void run() {
+		status.calcStatus();
 		double percent = status.getPercent();
 		StringBuilder builder = new StringBuilder();
 		
 		while(percent <= 1){
+			status.calcStatus();
 			builder.append(format.format((System.currentTimeMillis())));
 			builder.append("	");
 			builder.append(percent);
 			builder.append("	");
-			builder.append(status.getTimeRemaining());
+			long time = (System.currentTimeMillis() - status.getStartTime());
+			builder.append(" starttime: " + time);
+			builder.append(" time remaining: " + status.getTimeRemaining());
 			out.writeToFile(builder);
 			builder = new StringBuilder();
 			percent = status.getPercent();
 			
 			try {
 				synchronized (this) {
-					Thread.sleep(1000);
+					Thread.sleep(10000);
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
